1398db800ba06b2007ab78e3f047b7a8950ca874
tomih <tomi.haapakoski@helsinki.fi>
2019-02-10 00:00:39 +0200

    frontti lisätty

---
+++ build/precache-manifest.183d317626faf8256a79a1ff82ebb165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "649fa7e950dec9ae71fa",
+    "url": "/static/js/main.649fa7e9.chunk.js"
+  },
+  {
+    "revision": "d7e0a24a82cf4b61adc5",
+    "url": "/static/js/1.d7e0a24a.chunk.js"
+  },
+  {
+    "revision": "649fa7e950dec9ae71fa",
+    "url": "/static/css/main.bb961a54.chunk.css"
+  },
+  {
+    "revision": "7ad59629cf4f664eec6dfb46bbaef12f",
+    "url": "/index.html"
+  }
+];
