696140f90aaff999b5694f8906b9c16e55622694
tomih <tomi.haapakoski@helsinki.fi>
2019-02-09 23:16:26 +0200

    backend nettiin

--- index.js
+++ index.js
@@ -1,33 +1,113 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 
-let notes = [
+
+
+app.use(cors())
+morgan.token('body', function(req,res){return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(bodyParser.json())
+
+let persons = [
     {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
       },
       {
-        "name": "Martti Tienari",
-        "number": "040-123456",
-        "id": 2
+        name: "Martti Tienari",
+        number: "040-123456",
+        id: 2
       },
       {
-        "name": "Arto Järvinen",
-        "number": "040-123456",
-        "id": 3
+        name: "Arto Järvinen",
+        number: "040-123456",
+        id: 3
       },
       {
-        "name": "Lea Kutvonen",
-        "number": "040-1234569",
-        "id": 4
+        name: "Lea Kutvonen",
+        number: "040-1234569",
+        id: 4
       }
 ]
 
-const app = http.createServer((request, response)=>{
-    response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get('/', (req, res)=> {
+    res.send('<h1>Hello World</h1>')
 })
 
-const port = 3001
-app.listen(port)
-console.log(`server running on port ${port}`)
+app.get('/info', (req, res) =>{
+    
+    paiva = new Date();
+    const pituus = persons.length
+    const vyohyke = paiva.toUTCString()
+    res.send(`<p>Puhelinluettelossa on ${pituus} henkilön tiedot </p>
+    ${vyohyke}`)
+    
+
+    
+})
+
+app.get('/api/persons', (reqm, res) =>{
+    res.json(persons)
+})
+app.get('/api/persons/:id', (request, response) =>{
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+        response.json(person)
+    } else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) =>{
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end()
+})
+
+
+
+app.post('/api/persons', (request,response) =>{
+    const body = request.body
+    if(body.name === undefined || body.number === undefined){
+        return response.status(400).json({
+            error: 'Name and/or number missing'
+        })
+    }
+
+
+    const onLuettelossa = persons.find(person => person.name === body.name)
+
+
+    if(!onLuettelossa){
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random()*Math.floor(100000)),
+    }
+    
+    persons = persons.concat(person)
+
+    response.json(person)
+}else{
+    return response.status(400).json({
+        error: 'Person already exists'
+
+    })
+}
+})
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
