5eb787cb55c8264a5c0bc691d3126b3433bbe9d4
tomih <tomi.haapakoski@helsinki.fi>
2019-02-10 05:23:13 +0200

    herokkua yritetty korjata

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
-if(process.env.NODE_ENV !== 'production'){
-
-require('dotenv').config()
-}
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
